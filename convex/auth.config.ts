f8d69b206eda1b4f55a8174b3eda02b0e3067e90
gerard415 <chinedumike85@gmail.com>
2024-12-31 17:06:40 -0500

    convex-clerk set up

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://rational-bison-95.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  
+  export default authConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ConvexClerkProvider from "./providers/ConvexClerkProvider";
 
 export const metadata: Metadata = {
   title: "Podyssey",
@@ -16,10 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
+    <ConvexClerkProvider>
+      <html lang="en">
+        <body>
+            {children}
+        </body>
+      </html>
+    </ConvexClerkProvider>
+    
   );
 }
