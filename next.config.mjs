9cbc3dd789d1a1eafabeedbb065e6dde5350af9d
gerard415 <chinedumike85@gmail.com>
2025-01-03 11:43:29 -0500

    toaster

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'sleek-capybara-771.convex.cloud'
+        hostname: 'academic-goldfish-442.convex.cloud'
       },
       {
         protocol: 'https',
