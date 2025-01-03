63bc72f67ab7578ccb635b6feb5bf75b0d0a021d
gerard415 <chinedumike85@gmail.com>
2025-01-03 11:48:33 -0500

    css and auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,15 +1,17 @@
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-        <main>
-            <p className="text-white-1">LEFT SIDEBAR</p>
-            {children}
-            <p className="text-white-1">RIGHT SIDEBAR</p>
-        </main>
-    </div>
+    <main className="relative h-screen w-full">
+      <div className="absolute size-full">
+        <Image src="/images/bg-img.png" alt="background" fill className="size-full" />
+      </div>
+
+      {children}
+    </main>
   );
 }
