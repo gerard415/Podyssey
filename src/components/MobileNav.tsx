46a7f75681620f76965de0cba8f9f6a84584bc5e
gerard415 <chinedumike85@gmail.com>
2025-01-06 17:37:54 -0500

    small changes

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -30,7 +30,7 @@ const MobileNav = () => {
           <SheetDescription/>
           <Link href="/" className="flex cursor-pointer items-center gap-1 pb-10 pl-4">
             <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
-            <h1 className="text-24 font-extrabold  text-white-1 ml-2">Podcastr</h1>
+            <h1 className="text-24 font-extrabold  text-white-1 ml-2">Podyssey</h1>
           </Link>
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
